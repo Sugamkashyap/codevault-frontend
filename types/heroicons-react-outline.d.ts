5921c6fcfbdf5321d71dc81b40e2a728d472bcf7
Sugam Kashyap <sugamkashyap1@gmail.com>
2025-02-05 18:14:40 +0530

    Fix TypeScript declarations for @heroicons/react/outline

---
+++ types/heroicons-react-outline.d.ts
@@ -0,0 +1,8 @@
+declare module '@heroicons/react/outline' {
+  import * as React from 'react';
+  import { SVGProps } from 'react';
+
+  export const SearchIcon: React.FC<SVGProps<SVGSVGElement>>;
+  export const LogoutIcon: React.FC<SVGProps<SVGSVGElement>>;
+  // Add other icons you are using similarly
+} 
